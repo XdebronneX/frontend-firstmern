a0fe4214aa3248cfbf94fae41e8cbce1bb12e8d8
Xdebx <86518562+Xdebx@users.noreply.github.com>
2025-02-09 14:32:41 +0800

    update reducer

--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -44,14 +44,71 @@ import {
   USER_SALES_FAIL,
 } from "../constants/userConstants";
 
-export const authReducer = (state = { user: {} }, action) => {
+// export const authReducer = (state = { user: {} }, action) => {
+//   switch (action.type) {
+//     case REGISTER_USER_REQUEST:
+//     case LOGIN_REQUEST:
+//     case LOAD_USER_REQUEST:
+//       return {
+//         loading: true,
+//         isAuthenticated: false,
+//       };
+
+//     case REGISTER_USER_SUCCESS:
+//     case LOGIN_SUCCESS:
+//     case LOAD_USER_SUCCESS:
+//       return {
+//         ...state,
+//         loading: false,
+//         isAuthenticated: true,
+//         user: action.payload,
+//       };
+
+//     case REGISTER_USER_FAIL:
+//     case LOGIN_FAIL:
+//     case LOAD_USER_FAIL:
+//       return {
+//         ...state,
+//         loading: false,
+//         isAuthenticated: false,
+//         user: null,
+//         error: action.payload,
+//       };
+
+//     case LOGOUT_SUCCESS:
+//       return {
+//         loading: false,
+//         isAuthenticated: false,
+//         user: null,
+//       };
+
+//     case LOGOUT_FAIL:
+//       return {
+//         ...state,
+//         error: action.payload,
+//       };
+
+//     case CLEAR_ERRORS:
+//       return {
+//         ...state,
+//         error: null,
+//       };
+
+//     default:
+//       return state;
+//   }
+// };
+
+export const authReducer = (state = { user: null }, action) => {
   switch (action.type) {
     case REGISTER_USER_REQUEST:
     case LOGIN_REQUEST:
     case LOAD_USER_REQUEST:
       return {
+        ...state, // Keep previous state data
         loading: true,
         isAuthenticated: false,
+        user: null, // Ensure the user is null while loading
       };
 
     case REGISTER_USER_SUCCESS:
@@ -71,7 +128,7 @@ export const authReducer = (state = { user: {} }, action) => {
         ...state,
         loading: false,
         isAuthenticated: false,
-        user: null,
+        user: null, // User will be null if login or registration fails
         error: action.payload,
       };
 
